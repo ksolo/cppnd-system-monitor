e78cc746683c0a8b9831eba062fe0b8afa2fbf69
Kevin Solorio <soloriok@gmail.com>
2020-06-10 10:20:54 -0500

    wip: cpu utilizaton

--- include/processor.h
+++ include/processor.h
@@ -2,6 +2,7 @@
 #define PROCESSOR_H
 
 #include <fstream>
+#include <string>
 
 #define PROCESSOR_FILE_LOCATION "/proc/stat"
 
@@ -11,7 +12,21 @@ class Processor {
 
   // TODO: Declare any necessary private members
  private:
-  ifstream data_(PROCESSOR_FILE_LOCATION);
+  ifstream data_file_(PROCESSOR_FILE_LOCATION);
+
+  // Processor times
+  float user{0.0};
+  float nice{0.0};
+  float system{0.0};
+  float idle{0.0};
+  float iowait{0.0};
+  float irq{0.0};
+  float softirq{0.0};
+  float steal{0.0};
+  float guest{0.0};
+  float guest_nice{0.0};
+
+  void ParseCPULine(std::string const &cpu_data);
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,13 @@
 #include "processor.h"
 
+#include <sstream>
+
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  if (data_file_) {
+    std::string cpu_data = data_file_.getline();
+    ParseCPULine(cpu_data);
+  }
+}
+
+void Processor::ParseCPULine(std::string const &cpu_data) {}
